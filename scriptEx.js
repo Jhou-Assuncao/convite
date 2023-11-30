c7d8d64de70961461eabd0a746d4e601faa84570
Jhou Assunção <108472108+Jhou-Assuncao@users.noreply.github.com>
2023-11-30 17:48:31 -0300

    commit inicial

---
+++ scriptEx.js
@@ -0,0 +1,18 @@
+function receber() {
+    let nome = document.getElementById('txtnome')
+    let res = document.getElementById('res')
+
+    n = (nome.value)
+
+    if (n == 'Bruna' || n == 'BRUNA' || n == 'bruna') {
+        res.innerHTML = `<strong>${n}</strong>, quer se casar comigo?`
+        var img = document.createElement('img') //criar imagem
+        img.setAttribute('id', 'foto')
+        img.setAttribute('src', 'pedido1.jpg')
+        img.classList.add('img-fluid')
+
+    } else {
+        res.innerHTML = `Não há convites disponíveis para <strong>${n}</strong>!`
+    }
+    res.appendChild(img)
+}
