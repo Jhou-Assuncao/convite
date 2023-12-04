632e9fb4c6fbf650013e0a10bbbe6c364a4df10b
Jhou Assunção <108472108+Jhou-Assuncao@users.noreply.github.com>
2023-12-04 08:19:31 -0300

    commit 3
    Atualização de data

--- scriptEx.js
+++ scriptEx.js
@@ -1,10 +1,13 @@
 function receber() {
     let nome = document.getElementById('txtnome')
     let res = document.getElementById('res')
-
+    let dataAceite = new Date('2023-12-03')
+    let dataAtual = new Date()
+    let diferencaMilisegundos = dataAtual - dataAceite
+    let diferencaDias = Math.floor(diferencaMilisegundos / (1000 * 60 * 60 * 24))
     n = (nome.value)
 
-    if (n == 'Bruna' || n == 'BRUNA' || n == 'bruna') {
+    if (n == 'Bruna' || n == 'BRUNA' || n == 'bruna' || n == 'Bruna ') {
         res.innerHTML = `<p><strong>${n}</strong>, quer se casar comigo?</p>`
         var img = document.createElement('img') //criar imagem
         img.setAttribute('id', 'foto')
@@ -15,5 +18,6 @@ function receber() {
         res.innerHTML = `Não há convites disponíveis para <strong>${n}</strong>!`
     }
     res.appendChild(img)
+    document.getElementById('diferencaDias').innerText = `Hoje faz ${diferencaDias} dias`
 }
 
